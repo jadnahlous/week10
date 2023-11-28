a2de50d783c5777833fb8469708b679d44934716
jadnahlous <jadnahouson@gmail.com>
2023-11-28 09:47:27 +0400

    second commit

---
+++ checkTemperature.py
@@ -0,0 +1,9 @@
+import myTemperature as t
+
+c=int(input("Enter your temperature in Celsius and we will provide it to Fahrenheit"))
+
+print(c,"celsius is eq to",t.ctof(c))
+
+f=float(input("Enter your temperature in F and we will provide it to C"))
+
+print(f,"celsius is eq to",t.ftoc(f))
---
+++ myTemperature.py
@@ -0,0 +1,7 @@
+def ctof(ctemp):
+    ftemp=9/5*ctemp+32
+    return ftemp
+
+def ftoc(ftemp):
+    ctemp=(ftemp-32)*5/9
+    return ctemp
